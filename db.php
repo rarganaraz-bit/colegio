74b092f96fa3b01a970fdae3007ddd9076e8c58e
rarganaraz-bit <rarganaraz@alumnes.aulabalear.org>
2026-03-09 18:19:51 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "2007";
+$pass = "2007"; 
 $db   = "colegio";
 
 $conexion = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conexion) {
-    die("Error de conexión: " . mysqli_connect_error());
+    
+    die("Fallo de conexión: " . mysqli_connect_error());
 }
 ?>
